9a37e5a68fd361fa9a2053bd569d03d6e3bb403a
Gabe Cziprusz <gcziprusz@gmail.com>
2025-06-04 15:10:32 -0500

    math primer WIP

---
+++ src/chapters/just-enough-math/index.js
@@ -0,0 +1,48 @@
+import { polynomialsChapter } from './polynomials';
+import { exponentsLogarithmsChapter } from './exponents-logarithms';
+import { mathematicalFoundationsChapter } from './mathematical-foundations';
+import { glossaryChapter } from './glossary';
+
+export const justEnoughMathChapters = [
+  polynomialsChapter,
+  exponentsLogarithmsChapter,
+  mathematicalFoundationsChapter,
+  glossaryChapter
+];
+
+/**
+ * Get a chapter by its ID
+ * @param {string} id - The chapter ID to find
+ * @returns {Object|undefined} The chapter object if found, undefined otherwise
+ */
+export const getChapterById = (id) => {
+  return justEnoughMathChapters.find(chapter => chapter.id === id);
+};
+
+/**
+ * Get all chapter IDs for the just enough math section
+ * @returns {string[]} Array of chapter IDs
+ */
+export const getChapterIds = () => {
+  return justEnoughMathChapters.map(chapter => chapter.id);
+};
+
+/**
+ * Get the next chapter after the given chapter ID
+ * @param {string} currentChapterId - The current chapter ID
+ * @returns {Object|undefined} The next chapter object if found, undefined otherwise
+ */
+export const getNextChapter = (currentChapterId) => {
+  const currentIndex = justEnoughMathChapters.findIndex(chapter => chapter.id === currentChapterId);
+  return justEnoughMathChapters[currentIndex + 1];
+};
+
+/**
+ * Get the previous chapter before the given chapter ID
+ * @param {string} currentChapterId - The current chapter ID
+ * @returns {Object|undefined} The previous chapter object if found, undefined otherwise
+ */
+export const getPreviousChapter = (currentChapterId) => {
+  const currentIndex = justEnoughMathChapters.findIndex(chapter => chapter.id === currentChapterId);
+  return currentIndex > 0 ? justEnoughMathChapters[currentIndex - 1] : undefined;
+};
